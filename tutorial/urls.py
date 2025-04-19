6269d0e673225259308cfaaea38bbae713f2c548
Alden Sahi <106035261+AL-DN@users.noreply.github.com>
2025-04-19 10:39:44 -0400

    Update urls.py

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', include('blog.urls')),
    
     path('redirect-to-alpaca/', user_views.redirect_to_alpaca, name='redirect_to_alpaca'),
+    path('callback/', user_views.oauth_callback, name='alpaca_callback'),
 
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
